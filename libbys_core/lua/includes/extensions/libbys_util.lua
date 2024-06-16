384a03d26b010a7255e317f93e92a90c16f64961
Zach <147861361+1054009@users.noreply.github.com>
2024-06-16 05:03:53 -0400

    Add util.ForceType

--- libbys_core/lua/includes/extensions/libbys_util.lua
+++ libbys_core/lua/includes/extensions/libbys_util.lua
@@ -1,4 +1,9 @@
+local Angle = Angle
 local ErrorNoHaltWithStack = ErrorNoHaltWithStack
+local isvector = isvector
+local tobool = tobool
+local tonumber = tonumber
+local tostring = tostring
 local xpcall = xpcall
 
 local function PrintCallbackError(message)
@@ -7,4 +12,30 @@ end
 
 function util.SafeCallback(callback, ...)
 	xpcall(callback, PrintCallbackError, ...)
+end
+
+function util.ForceType(value, force)
+	if force == FORCE_STRING then
+		return tostring(value)
+	elseif force == FORCE_NUMBER then
+		return tonumber(value) or 0
+	elseif force == FORCE_BOOL then
+		return tobool(value)
+	elseif force == FORCE_ANGLE then
+		return Angle(value)
+	elseif force == FORCE_COLOR then
+		if isvector(value) then
+			return value:ToColor()
+		else
+			return string.ToColor(tostring(value))
+		end
+	elseif force == FORCE_VECTOR then
+		if IsColor(value) then
+			return value:ToVector()
+		else
+			return Vector(value)
+		end
+	else
+		return value
+	end
 end
